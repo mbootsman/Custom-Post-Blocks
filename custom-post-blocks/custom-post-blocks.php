49a949a0cb9626c114d9d75fad3946e59f4c6342
Marcel Bootsman <marcel@nostromo.nl>
2018-01-24 20:28:52 +0100

    Initial commit

---
+++ custom-post-blocks/custom-post-blocks.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+* Plugin Name: Custom Post Blocks
+* Plugin URI: https://custompostblocks.com/
+* Description: A Gutenberg add-on for using Custom Post Types in Gutenberg blocks.
+* Author: Marcel Bootsman
+* Author URI: https://marcelbootsman.nl/
+* Version: 1.0.0
+* License: GPL2+
+* License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+*
+* @package CPB
+*/
